df7f30230a5417056d3d771d1a2038632e208f37
Dani Santos <santos.adaniele@gmail.com>
2020-04-08 10:38:33 -0400

    basic redux setup - exercises 1 and 2

--- workshop/server/server.js
+++ workshop/server/server.js
@@ -1,6 +1,8 @@
-const path = require('path');
-const express = require('express');
-const bodyParser = require('body-parser');
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+require("es6-promise").polyfill();
+const fetch = require("isomorphic-fetch");
 
 const app = new express();
 const port = 5678;
@@ -8,21 +10,32 @@ const port = 5678;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/spotify_access_token', (req, res, next) => {
+app.get("/spotify_access_token", async (req, res, next) => {
   const clientId = process.env.SPOTIFY_CLIENT_ID;
   const clientSecret = process.env.SPOTIFY_SECRET;
 
   // We need, annoyingly, a base64-encoded string of our id:secret, for spotify.
   // We can use Buffers to do this for us.
-  const authString = Buffer.from(clientId + ':' + clientSecret).toString(
-    'base64'
+  const authString = Buffer.from(clientId + ":" + clientSecret).toString(
+    "base64"
   );
-
-  // TODO: use authString in a request to Spotify!
-  res.send({ todo: true });
+  try {
+    const response = await fetch("https://accounts.spotify.com/api/token", {
+      method: "POST",
+      headers: {
+        Authorization: `Basic ${authString}`,
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: "grant_type=client_credentials",
+    });
+    const json = await response.json();
+    return res.send(json);
+  } catch (e) {
+    res.send(500).send({ error: e });
+  }
 });
 
-app.listen(port, function(error) {
+app.listen(port, function (error) {
   if (error) {
     console.error(error);
   } else {
